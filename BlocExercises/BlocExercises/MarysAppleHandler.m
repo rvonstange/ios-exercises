c6ac87f45978b92813baccdab10034116dd8d76a
Robert von Stange <rvonstan@andrew.cmu.edu>
2015-09-23 20:42:31 -0400

    Checkpoint 11 complete

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -13,7 +13,21 @@
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
     NSString *itemToReturn;
 
-    /* WORK HERE */
+    if (dollars < 5) {
+        itemToReturn = @"get out of my store";
+    }
+    else if (dollars == 5) {
+        itemToReturn = @"have some gum";
+    }
+    else if (dollars == 6) {
+        itemToReturn = @"have an apple";
+    }
+    else if ((dollars >= 1000) && (dollars < 1000000000)) {
+        itemToReturn = @"have an Apple computer";
+    }
+    else if (dollars >= 1000000000) {
+        itemToReturn = @"have The Big Apple";
+    }
     
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
@@ -24,11 +38,8 @@
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    return (self.getsDiscount) ? (cost*.75) : cost;
     
-    return cost;
 }
 
 @end
